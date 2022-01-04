fef54969404097a03cda649a79b99671e555475b
Emmanuel Carvente Gonzalez <carvente.e@mexrentacar.com>
2022-01-03 21:33:37 -0600

    Se agrego el contendido de carpeta vistas con 2 vistas

---
+++ vistas/modificar.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <title> MODIFICAR REGISTRO</title>
+</head>
+<body>
+    <?php
+        $id= $_GET['id'];
+    ?>
+    <div class="flex-container text-center align-items-center ">
+        <form action="http://localhost/CrudSimple/?id=<?php echo ($id); ?>" method="POST">
+            <h2 class="h2">Modifique los datos que Desea Cambiar</h2>
+            <label for="nombre">Lenguaje</label><input type="text" name="Mlengua" value="<?php  echo $_GET['name']?>">
+            <label for="lenguaje">Pais</label><input type="text" name="Mpais" value ="<?php echo $_GET['id'] ?>">
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ vistas/nuevo-form.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">  
+    <title>Agregar Registro</title>
+</head>
+<body>  
+    <div class="d-flex align-self-center justify-content-center mt-5 border">
+        <form method="POST" action="http://localhost/CrudSimple/">
+            <label for="nombre">Nombre</label> <input type="text" name="nombre">
+            <label for="lenguaje">Lenguaje</label> <input type="text" name="lenguaje">
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</body>
+</html>
