de790efa10e367d2eac1aa6bf2bfebf737ce24b4
Emmanuel Carvente Gonzalez <carvente.e@mexrentacar.com>
2022-01-05 11:52:57 -0600

    Verificamos que los datos no sean vacios al modificar el pais

--- vistas/modificar.php
+++ vistas/modificar.php
@@ -11,10 +11,10 @@
         $id= $_GET['id'];
     ?>
     <div class="flex-container text-center align-items-center ">
-        <form action="http://localhost/CrudSimple/?id=<?php echo ($id); ?>" method="POST">
+        <form action="http://localhost/CrudSimple/?id=<?php echo ($id); ?>"method="POST">
             <h2 class="h2">Modifique los datos que Desea Cambiar</h2>
-            <label for="nombre">Lenguaje</label><input type="text" name="Mlengua" value="<?php  echo $_GET['name']?>">
-            <label for="lenguaje">Pais</label><input type="text" name="Mpais" value ="<?php echo $_GET['id'] ?>">
+            <label for="nombre">Lenguaje</label><input type="text" name="Mlengua" value="<?php  echo $_GET['name']?>" required>
+            <label for="lenguaje">Pais</label><input type="text" name="Mpais" value ="<?php echo $_GET['id'] ?>" required>
             <button type="submit">Enviar</button>
         </form>
     </div>
